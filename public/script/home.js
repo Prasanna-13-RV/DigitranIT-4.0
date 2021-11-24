979f572b75ec3b43bc240aa1aee67892b62e1e1b
lohithsrk <srklohith05@gmail.com>
2021-11-24 13:13:51 +0530

    Changes

--- public/script/home.js
+++ public/script/home.js
@@ -8,5 +8,13 @@ const t1 = gsap.timeline({
 		markers: true
 	}
 });
-// t1.fromTo('.d-effect', { x: 0 }, { x: '-5000rem' });
-t1.fromTo('.d', { fontSize: '15rem' }, { fontSize: '1000rem', duration: 0.5 });
+t1.fromTo(
+	'.d',
+	{ width: '20rem', rotate: '0deg' },
+	{ width: '35rem', rotate: '180deg' }
+);
+t1.fromTo(
+	'.d',
+	{ width: '35rem', rotate: '180deg' },
+	{ width: '20rem', rotate: '360deg' }
+);
