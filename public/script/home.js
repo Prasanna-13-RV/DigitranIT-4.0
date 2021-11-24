39134d9235442a94eacfdbb08ce6e852fc69569d
lohithsrk <srklohith05@gmail.com>
2021-11-24 11:21:42 +0530

    changes

--- public/script/home.js
+++ public/script/home.js
@@ -1,1 +1,12 @@
-const t1 = gsap.timeline({scrollTer})
+const t1 = gsap.timeline({
+	scrollTrigger: {
+		trigger: '.d-effect',
+		start: 'top',
+		end: '100%',
+		scrub: 'true',
+		pin: true,
+		markers: true
+	}
+});
+// t1.fromTo('.d-effect', { x: 0 }, { x: '-5000rem' });
+t1.fromTo('.d', { fontSize: '15rem' }, { fontSize: '1000rem', duration: 0.5 });
