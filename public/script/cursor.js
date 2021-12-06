20c14e460e6ebea4cbecac4f2f320543a6c20398
Prasanna RV <prasannavelmurugan0200@gmail.com>
2021-12-06 09:31:21 +0530

    chages

--- public/script/cursor.js
+++ public/script/cursor.js
@@ -1,23 +1,21 @@
 const cursor = document.querySelector('.cursor');
 const cardDivImage = document.querySelectorAll('.card-div-image');
 
-
-cardDivImage.forEach(element => {
-    element.addEventListener('mouseover', (e) => {
-        cursor.classList.add('cursor-active');
-        x = e.pageX;
-        y = e.pageY;
-        cursor.style.top = y + 'px';
-        cursor.style.left = x + 'px';
+const first_image_section = (card_image, cursor_div) => {
+    card_image.forEach(element => {
+        element.addEventListener('mouseover', (e) => {
+            cursor_div.classList.add('cursor-active');
+            x = e.pageX;
+            y = e.pageY;
+            cursor.style.top = y + 'px';
+            cursor.style.left = x + 'px';
+        });
+        element.addEventListener('mouseleave', (e) => {
+            cursor.classList.remove('cursor-active');
+        });
     });
-    element.addEventListener('mouseleave', (e) => {
-        cursor.classList.remove('cursor-active');
-    });
-});
-
-
-
-
+}
+first_image_section(cardDivImage, cursor);
 
 document.addEventListener('mousemove', (e) => {
     x = e.pageX;
