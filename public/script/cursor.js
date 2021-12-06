59b72041fdf5085b916d6ac1bbaf8164ff695f9c
Prasanna RV <prasannavelmurugan0200@gmail.com>
2021-12-06 10:50:54 +0530

    changes

--- public/script/cursor.js
+++ public/script/cursor.js
@@ -1,21 +1,20 @@
 const cursor = document.querySelector('.cursor');
 const cardDivImage = document.querySelectorAll('.card-div-image');
 
-const first_image_section = (card_image, cursor_div) => {
-    card_image.forEach(element => {
-        element.addEventListener('mouseover', (e) => {
-            cursor_div.classList.add('cursor-active');
-            x = e.pageX;
-            y = e.pageY;
-            cursor.style.top = y + 'px';
-            cursor.style.left = x + 'px';
-        });
-        element.addEventListener('mouseleave', (e) => {
-            cursor.classList.remove('cursor-active');
-        });
+
+cardDivImage.forEach(element => {
+    element.addEventListener('mouseover', (e) => {
+        cursor.classList.add('cursor-active');
+        x = e.pageX;
+        y = e.pageY;
+        cursor.style.top = y + 'px';
+        cursor.style.left = x + 'px';
     });
-}
-first_image_section(cardDivImage, cursor);
+    element.addEventListener('mouseleave', (e) => {
+        cursor.classList.remove('cursor-active');
+    });
+});
+
 
 document.addEventListener('mousemove', (e) => {
     x = e.pageX;
